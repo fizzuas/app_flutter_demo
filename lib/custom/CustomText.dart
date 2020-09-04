21062755f8be666ff72c4164ff78245d73ae0378
fizzuas <1605639412@qq.com>
2020-09-04 18:21:22 +0800

    doalog 状态更新

--- lib/custom/CustomText.dart
+++ lib/custom/CustomText.dart
@@ -9,7 +9,14 @@ class CustomText extends StatefulWidget {
 
 class _CustomTextState extends State<CustomText> {
   @override
+  void initState() {
+    super.initState();
+
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return Container(
         child: RepaintBoundary(
       child: CustomPaint(
@@ -18,6 +25,8 @@ class _CustomTextState extends State<CustomText> {
       ),
     ));
   }
+
+
 }
 
 class TextPaint extends CustomPainter {
@@ -26,15 +35,27 @@ class TextPaint extends CustomPainter {
     ..isAntiAlias = true
     ..style = PaintingStyle.fill;
 
+  Paint _paintLine = new Paint()
+    ..color = Colors.black
+    ..isAntiAlias = true;
+
+  Paint keyPaint = Paint()
+    ..isAntiAlias = true
+    ..style = PaintingStyle.fill
+    ..color = Color(0xFFEEEEEE);
+
   @override
   void paint(Canvas canvas, Size size) {
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), _paint);
+
+    canvas.drawLine(Offset(50, 0), Offset(50, 50), _paintLine);
+
     final textStyle = TextStyle(
       color: Colors.black,
       fontSize: 30,
     );
     final textSpan = TextSpan(
-      text: 'Hello, world.',
+      text: 'A',
       style: textStyle,
     );
     final textPainter = TextPainter(
@@ -42,13 +63,32 @@ class TextPaint extends CustomPainter {
       textDirection: TextDirection.ltr,
     );
     textPainter.layout();
-    canvas.save();
-    print("text height"+textPainter.height.toString());
-    canvas.translate(textPainter.height/2, 0);
-    canvas.rotate(pi / 2);
-    final offset = Offset(0, 0);
+    print("text height" + textPainter.height.toString());
+    final offset =
+        Offset(50 - textPainter.width / 2, 50 - textPainter.height / 2);
     textPainter.paint(canvas, offset);
+
+    // 旋转
+    canvas.drawLine(Offset(100, 0), Offset(100, 50), _paintLine);
+    canvas.save();
+    canvas.translate(100, 50);
+    canvas.rotate(-pi / 2);
+    textPainter.paint(
+        canvas, Offset(-textPainter.width / 2, -textPainter.height / 2));
     canvas.restore();
+
+    _drawLocatePoint(canvas, size, Offset(50, 50));
+  }
+
+  //画定位点的箭头
+  void _drawLocatePoint(Canvas canvas, Size size, Offset center) {
+    keyPaint.strokeWidth = 1.5;
+    canvas.drawLine(Offset(center.dx, center.dy),
+        Offset(center.dx, center.dy - 14), keyPaint);
+    canvas.drawLine(Offset(center.dx, center.dy),
+        Offset(center.dx - 2, center.dy - 4), keyPaint);
+    canvas.drawLine(Offset(center.dx, center.dy),
+        Offset(center.dx + 2, center.dy - 4), keyPaint);
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/ColumnSection.dart';
 import 'package:flutter_app/GestureDemo.dart';
@@ -10,13 +12,130 @@ import 'KeyMap.dart';
 import 'custom/CustomText.dart';
 import 'custom/RoateText.dart';
 
-void main() => runApp(new MaterialApp(
+void main() => runApp(new MyAPP());
+
+class MyAPP extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyState<MyApp>();
+  }
+}
+
+class MyState<MyApp> extends State {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "ss",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("ss"),
+          title: Text("dd"),
         ),
-        body: Container(
-          child: CustomText(),
-        ),
+        body: new MyBody(),
       ),
-    ));
+    );
+  }
+}
+
+class MyBody extends StatefulWidget {
+  @override
+  MyBodyState createState() => MyBodyState();
+}
+
+StateSetter ss;
+int mDialogState = 0;
+
+class MyBodyState extends State<MyBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          FlatButton(
+            color: Colors.grey,
+            onPressed: () {
+              showTextDialog(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Future<bool> showTextDialog(BuildContext context) {
+  var sb = StatefulBuilder(
+    builder: (ctx, state) {
+      ss = state;
+
+      if (mDialogState == 0) {
+        return Center(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.grey,
+                  onPressed: () {
+                    onFlatButClick();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      } else {
+        return Center(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    onFlatButClick();
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    },
+  );
+  return showDialog(context: context, builder: (ctx) => sb);
+}
+
+void onFlatButClick() {
+  print("onFlatButton click");
+  ss(() {
+    mDialogState = (mDialogState + 1) % 2;
+  });
+}
+
+_showDialogWithStatefulBuilder(BuildContext context) {
+  var progress = 0.0;
+  StateSetter ss;
+  Timer.periodic(Duration(milliseconds: 300), (timer) {
+    progress += 0.1;
+    if (ss != null) {
+      ss(() {});
+    }
+    if (progress >= 1) {
+      timer.cancel();
+      ss = null;
+    }
+  });
+  var sb = StatefulBuilder(
+    builder: (ctx, state) {
+      ss = state;
+      return Center(
+        child: Container(
+          height: 40,
+          child: LinearProgressIndicator(
+            backgroundColor: Colors.white,
+            value: progress,
+          ),
+        ),
+      );
+    },
+  );
+  showDialog(context: context, builder: (ctx) => sb);
+}
